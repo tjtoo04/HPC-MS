989f17a91441ee861fe890d4d69591f3a0e98c14
Tze <ttjiat@gmail.com>
2025-11-17 12:29:17 +0800

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -183,39 +183,6 @@ void showMainMenu()
 int main(int argc, char *argv[]) {
   enableColorSupport();
 
-  // std::cout << "We Are So Cooked";
-
-  // const int MAX_LINES = 5;
-  // std::string data[MAX_LINES] = {
-  //     "Alice",
-  //     "Bob",
-  //     "Charlie",
-  //     "David",
-  //     "Eve",
-  // };
-
-  // // Write to file
-  // if (FileIO::writeToFile("patients.txt", data, MAX_LINES))
-  //   std::cout << "File written successfully!\n";
-  // else
-  //   std::cout << "Failed to write file.\n";
-
-  // // Read back
-  // std::string readBuffer[MAX_LINES];
-  // int linesRead = FileIO::readFromFile("patients.txt", readBuffer,
-  // MAX_LINES);
-
-  // if (linesRead > 0)
-  // {
-  //   std::cout << "Read " << linesRead << " lines:\n";
-  //   for (int i = 0; i < linesRead; ++i)
-  //     std::cout << readBuffer[i] << "\n";
-  // }
-  // else
-  // {
-  //   std::cout << "Failed to read file.\n";
-  // }
-
   showMainMenu();
 
   return 0;
